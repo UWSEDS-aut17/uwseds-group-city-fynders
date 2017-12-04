f500e76b6f2225d0adbd406f5e151272152cd49e
Zhuochen Han <zchan@uw.edu>
2017-12-03 22:55:08 -0800

    change doc dir name

---
+++ doc/Data.md
@@ -0,0 +1,27 @@
+## Requirements
+To fulfill the objectives we set in README.md, which is to create a city-ranking tool to select the most livable city according to four categories of data: **environment, human, economy and entertainment**, we need to find data of all major US cities in these four areas.
+
+## Resources
+1. **Environment** related data: Multi-year climate: [Monthly average temperature, annual average precipitation, snowfall average] https://www.infoplease.com/science-health/weather/climate-100-selected-us-cities
+Air and water quality: https://www.rd.com/advice/travel/50-cleanest-dirtiest-cities-in-america/
+2. **Human** related data: Crime rate :https://en.wikipedia.org/wiki/List_of_United_States_cities_by_crime_rate
+Unemployment Rates for Metropolitan Areas: https://www.bls.gov/web/metro/laummtrk.htm#laummtrk.f.p
+3. **Economy** related data: Cost of living in US: https://www.expatistan.com/cost-of-living/country/united-states#all-cities Tax rates: https://taxfoundation.org/sales-tax-rates-major-cities-midyear-2017/
+4. **Entertainment** related data: number of restaurants, museums, libraries, park area per person
+https://en.wikipedia.org/wiki/List_of_science_centers_in_the_United_States
+
+## Evaluation
+|                         | Environment | Human|Economy|Entertainment|
+|-------------------------|:-----------:|:----:|:-----:|:-----------:|
+|Seasonal avg. temperature|      x      |      |       |             |
+|Annual avg. precipitation|      x      |      |       |             |
+|Snowfall avg.            |      x      |      |       |             |
+|Air and water quality    |      x      |      |       |             |
+|Crime rate               |             |  x   |       |             |
+|Avg. household income    |             |      |   x   |             |
+|Unemployed rate          |             |      |   x   |             |
+|Living cost              |             |      |       |      x      |
+|City-owned parks         |             |      |       |      x      |
+|Bar count                |             |      |       |      x      |
+|Restaurant count         |             |      |       |      x      |
+|museums count            |             |      |       |      x      |
---
+++ doc/data_processing.py
@@ -0,0 +1,128 @@
+import pandas as pd
+import numpy as np
+
+
+def read_data():
+    # Read data from github
+    natural = pd.read_csv('../data/Natural.csv')
+    human = pd.read_csv('../data/human_related.csv')
+    economy = pd.read_csv('../data/economy.csv')
+    tertiary = pd.read_csv("../data/tertiary.csv")
+
+    return natural, human, economy, tertiary
+
+
+def data_rank(natural, human, economy, tertiary):
+
+    #natural
+    natural['Air'] = natural['Air'].rank(ascending=0)
+    natural['Water_quality'] = natural['Water_quality'].rank(ascending=0)
+    natural['Toxics'] = natural['Toxics'].rank(ascending=0)
+    natural['Hazardous'] = natural['Hazardous'].rank(ascending=0)
+    natural['Green_score_rank'] = natural['Green_score'].rank(ascending=1)
+    natural['Green_score_rank'].fillna(natural['Green_score_rank'].max()+1, inplace=True)
+    natural['Sanitation'].fillna(natural['Sanitation'].max()+1, inplace=True)
+    natural['Natural_total_score'] = (natural['Air'] + natural['Water_quality'] + natural['Toxics']
+                                      + natural['Hazardous'] + natural['Green_score_rank'])
+    natural['Natural_total_rank'] = natural['Natural_total_score'].rank(ascending=1)
+
+    #human related
+    human['total crime'] = human['Violent'] + human['Rape'] + human['Robbery']
+    human['crime_rank'] = human['total crime'].rank(ascending=1)
+    # Fill the NaN data to the max ranking + 1 with non-NaN data.
+    human['crime_rank'].fillna(human['crime_rank'].max() + 1,inplace=True)
+
+    human['hospital_rank']= human['NumHospital'].rank(ascending=0)
+    human['hospital_rank'].fillna(human['hospital_rank'].max() + 1,inplace=True)
+
+    human['early_education_rank'] = human['AvgSATScore'].rank(ascending=0)
+    human['early_education_rank'].fillna(human['early_education_rank'].max() + 1,inplace=True)
+
+    human['Population'] = pd.to_numeric(human['Population'], errors ='coerce')
+    a = np.multiply(human['Percent_graduate_degree'], human['Population'])
+    a = a.rank(ascending=0)
+    b = human['Colleges'].rank(ascending=0)
+    human['advanced_education_rank'] = (a + b).rank(ascending=1)
+    human['advanced_education_rank'].fillna(human['advanced_education_rank'].max() + 1,inplace=True)
+
+    human['Human_related_rank'] = (human['crime_rank'] + human['hospital_rank'] + human['early_education_rank'] +\
+                                   human['advanced_education_rank']).rank(ascending=1)
+
+    #economy
+    economy['Rank_Unemployment'] = economy['Percent unemployment'].rank(ascending = 1)
+    economy['Rank_Sales'] = economy['Local tax rate'].rank(ascending = 1)
+    economy['Rank_Income'] = economy['Median Income'].rank(ascending = 0)
+    economy['Rank_Tuition'] = economy['AvgTuition'].rank(ascending = 1)
+    economy['Rank_Unemployment'].fillna(economy['Rank_Unemployment'].max() + 1, inplace=True)
+    economy['Rank_Sales'].fillna(economy['Rank_Sales'].max() + 1, inplace=True)
+    economy['Rank_Income'].fillna(economy['Rank_Income'].max() + 1, inplace=True)
+    economy['Rank_Tuition'].fillna(economy['Rank_Tuition'].max() + 1, inplace=True)
+    economy['Sum'] = (economy['Rank_Unemployment'] + economy['Rank_Sales'] +
+                      economy['Rank_Income'] + economy['Rank_Tuition'])
+    economy['Economy_rank'] = economy['Sum'].rank(ascending = 1)
+
+    #tertiary
+    tertiary['Bar_Rank'] = tertiary['Bars'].rank(ascending = 0)
+    tertiary['Restaurant_Rank'] = tertiary['Restaurant'].rank(ascending = 0)
+    tertiary['Museums_Rank'] = tertiary['Museums'].rank(ascending = 0)
+    tertiary['Libraries_Rank'] = tertiary['Libraries'].rank(ascending = 0)
+    tertiary['Park_Rank'] = tertiary['Park_acres_per_1000_residents'].rank(ascending = 0)
+    tertiary['TopRes_Rank'] = tertiary['NumTop200Restau'].rank(ascending = 0)
+    tertiary['Bar_Rank'].fillna(tertiary['Bar_Rank'].max() + 1,inplace=True)
+    tertiary['Restaurant_Rank'].fillna(tertiary['Restaurant_Rank'].max() + 1,inplace=True)
+    tertiary['Museums_Rank'].fillna(tertiary['Museums_Rank'].max() + 1,inplace=True)
+    tertiary['Libraries_Rank'].fillna(tertiary['Libraries_Rank'].max()+1,inplace=True)
+    tertiary['Park_Rank'].fillna(tertiary['Park_Rank'].max() + 1,inplace=True)
+    tertiary['TopRes_Rank'].fillna(tertiary['TopRes_Rank'].max() + 1,inplace=True)
+    tertiary['Total_Rank'] = tertiary['Bar_Rank'] + tertiary['Restaurant_Rank'] + tertiary['Museums_Rank'] +\
+                            tertiary['Libraries_Rank'] + tertiary['Park_Rank'] + tertiary['TopRes_Rank']
+    tertiary['Tertiary_Rank'] = tertiary['Total_Rank'].rank(ascending = 1)
+
+    return natural, human, economy, tertiary
+
+
+def create_rank(natural, human, economy, tertiary, Lat, Lon):
+
+    rank = pd.DataFrame()
+
+    # get natural-relate rank
+    rank['Air'] = natural['Air']
+    rank['Water'] = natural['Water_quality']
+    rank['Toxics'] = natural['Toxics']
+    rank['Hazardous'] = natural['Hazardous']
+    rank['Green_score'] = natural['Green_score_rank']
+    rank['Natural_total_rank'] = natural['Natural_total_rank']
+
+    # get human-relate rank
+    rank['City'] = human['City']
+    rank['State'] = human['State']
+    rank['Population'] = human['Population']
+    rank['Crime_rank'] = human['crime_rank']
+    rank['Hospital_rank'] = human['hospital_rank']
+    rank['Early_education_rank'] = human['early_education_rank']
+    rank['University_education_rank'] = human['advanced_education_rank']
+    rank['Human_related_rank'] = human['Human_related_rank']
+
+    # get economy-relate rank
+    rank['Rank_unemployment'] = economy['Rank_Unemployment']
+    rank['Rank_sale_rate'] = economy['Rank_Sales']
+    rank['Rank_Income'] = economy['Rank_Income']
+    rank['Rank_Tuition'] = economy['Rank_Tuition']
+    rank['Economy_rank'] = economy['Economy_rank']
+
+    # get tertiary-relate rank
+    rank['Bar_Rank'] = tertiary['Bar_Rank']
+    rank['Restaurant_Rank'] = tertiary['Restaurant_Rank']
+    rank['Museums_Rank'] = tertiary['Museums_Rank']
+    rank['Libraries_Rank'] = tertiary['Libraries_Rank']
+    rank['Park_Rank'] = tertiary['Park_Rank']
+    rank['TopRes_Rank'] = tertiary['TopRes_Rank']
+    rank['Tertiary_Rank'] = tertiary['Tertiary_Rank']
+
+    # get location information
+    rank['Latitude'] = Lat
+    rank['Longitude'] = Lon
+
+    rank.to_csv("../data/rank_file.csv")
+
+    return rank
