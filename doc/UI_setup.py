06ae331034871e4b2f6fe9195e1406802868f32f
xiangyu <zhang3x4@uw.edu>
2017-12-03 23:50:26 -0800

    User Interface python file

--- doc/UI_setup.py
+++ doc/UI_setup.py
@@ -4,17 +4,21 @@ import dash_html_components as html
 import pandas as pd
 
 def layout_setup(pairs):
+    """
+    This function returns a layout of the user interface
+
+    pairs: pairs is a two_dimensional list. The first is the columns from pandas data frame,
+           the second is the relative name for each column in the dropdown choices
+    """
+
     lay = html.Div([
         html.Div([
             html.Br(),
             html.Br(),
             html.Center(html.H1("Find Your Dream City to Live in the US",
-                                style={'color': 'lavender', 'fontFamily': 'Helvetica', 'fontSize': 50}
-                               ),
+                                style={'color': 'lavender', 'fontFamily': 'Helvetica', 'fontSize': 50}),
                        ),
-
-            html.Center(
-                html.P("Powerd by City Fynders",
+            html.Center(html.P("Powerd by City Fynders",
                                style={'color': 'black','fontFamily':'Helvetica', 'fontSize': 20}),
                        ),
             html.Br(),
@@ -26,8 +30,8 @@ def layout_setup(pairs):
             html.Br(),
             html.Center(html.Img(src="http://travelquaz.com/wp-content/uploads/2015/09/photo-travel-holidays-to-the-usa-america-visit-usa-images.jpg"))
         ]),
-        html.Center(html.Div([
-            html.P("Ranking by all the fators",
+
+        html.Center(html.Div([html.P("Ranking by all the fators",
                            style={'color': 'black','fontFamily':'Helvetica', 'fontSize': 20}),
         ])),
 
@@ -38,109 +42,90 @@ def layout_setup(pairs):
 
         html.Center(html.Div([
             html.P("Ranking by four main factors separately",
-                           style={'color': 'black','fontFamily':'Helvetica', 'fontSize': 20}),
-
+                style={'color': 'black','fontFamily':'Helvetica', 'fontSize': 20}),
         ])),
 
         html.Center(html.Div([
-
                 html.Button('Human Related Rank', id='Hrank',
                                 style={'width': 200, 'color': 'black', 'backgroundColor': 'lightblue', 'height': 70,'fontSize': 15,'marginRight':100}),
-
                 html.Button('Natural Rank', id='Nrank',
                                 style={'width': 200, 'color': 'black', 'backgroundColor': 'lightblue', 'height': 70,'fontSize': 15,'marginRight':100}),
-
                 html.Button('Economy Rank', id='Erank',
                                 style={'width': 200, 'color': 'black', 'backgroundColor': 'lightblue', 'height': 70,'fontSize': 15,'marginRight':100}),
-
                 html.Button('Teritary Industry Rank', id='Trank',
                                 style={'width': 200, 'color': 'black', 'backgroundColor': 'lightblue', 'height': 70,'fontSize': 15}),
         ])),
-
-
-            # First important factor
-            html.Div([
-
-                html.P('Important No.1'),
-
-                dcc.Dropdown(
-                    id='First-care',
-                    # Set the labels and values
-                    options=[ {'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
-                )
-                ],
+        # First important factor
+        html.Div([
+            html.P('Important No.1'),
+            dcc.Dropdown(
+                id='First-care',
+                options=[ {'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
+            )
+        ],
             style={'width': '20%', 'fontFamily':'Helvetica','display': 'inline-block'}),
-
-
-            # Second important factor
-            html.Div([
-
-                html.P('Important No.2'),
-
-                dcc.Dropdown(
-                    id='Second-care',
-                    options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
-
-                )
-                ],
+        # Second important factor
+        html.Div([
+            html.P('Important No.2'),
+            dcc.Dropdown(
+                id='Second-care',
+                options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
+            )
+        ],
             style={'width': '20%','fontFamily':'Helvetica', 'display': 'inline-block'}),
-
-            # Third important factor
-            html.Div([
-
-                html.P('Important No.3'),
-
-                dcc.Dropdown(
-                    id='Third-care',
-                    options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
-
-                )
-                ],
+        # Third important factor
+        html.Div([
+            html.P('Important No.3'),
+            dcc.Dropdown(
+                id='Third-care',
+                options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
+            )
+        ],
             style={'width': '20%','fontFamily':'Helvetica', 'display': 'inline-block'}),
-
-            # Fourth important factor
-            html.Div([
-
-                html.P('Important No.4'),
-
-                dcc.Dropdown(
-                    id='Fourth-care',
-                    options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
-
-                )
-                ],
+        # Fourth important factor
+        html.Div([
+            html.P('Important No.4'),
+            dcc.Dropdown(
+                id='Fourth-care',
+                options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
+            )
+        ],
             style={'width': '20%','fontFamily':'Helvetica', 'display': 'inline-block'}),
-
-            # Fifth important factor
-            html.Div([
-
-                html.P('Important No.5'),
-
-                dcc.Dropdown(
-                    id='Fifth-care',
-                    options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
-
-                )
-                ],
+        # Fifth important factor
+        html.Div([
+            html.P('Important No.5'),
+            dcc.Dropdown(
+                id='Fifth-care',
+                options=[{'label': pairs[1][i],'value': pairs[0][i]} for i in range(19)],
+            )
+        ],
             style={'width': '20%', 'fontFamily':'Helvetica','display': 'inline-block'}),
-
-            html.Br(),
-            html.Br(),
-            html.Br(),
-
-            html.Center(html.Button('Go Searching!', id='Search',
-                                    style={'width': 200, 'color': 'white', 'backgroundColor': 'FireBrick', 'height': 70,'fontSize': 20})),
-
-            html.Div(dcc.Graph(id='User-graphic')),
-            html.Div(dcc.Graph(id='Total-graphic')),
-            html.Div(dcc.Graph(id='Human-graphic')),
-            html.Div(dcc.Graph(id='Natural-graphic')),
-            html.Div(dcc.Graph(id='Economic-graphic')),
-            html.Div(dcc.Graph(id='Tertiary-graphic'))
-        ])
+        html.Br(),
+        html.Br(),
+        html.Br(),
+        html.Center(html.Button('Go Searching!', id='Search',
+            style={'width': 200, 'color': 'white', 'backgroundColor': 'FireBrick', 'height': 70,'fontSize': 20})),
+        html.Div(dcc.Graph(id='User-graphic')),
+        html.Div(dcc.Graph(id='Total-graphic')),
+        html.Div(dcc.Graph(id='Human-graphic')),
+        html.Div(dcc.Graph(id='Natural-graphic')),
+        html.Div(dcc.Graph(id='Economic-graphic')),
+        html.Div(dcc.Graph(id='Tertiary-graphic'))
+    ])
     return lay
 
 def newdf(rank, First_care, Second_care, Third_care, Fourth_care, Fifth_care):
+    """
+    This function returns a new data frame from user choices
+
+    rank: pandas data frame
+    First_care: the first care column name chose from rank
+    Second_care: the second care column name chose from rank
+    Third_care: the third care column name chose from rank
+    Fourth_care: the fourth care column name chose from rank
+    Fifth_care: the fifth care column name chose from rank
+    """
+
     df = pd.DataFrame()
     df['City'] = rank['City']
     df['First'] = rank[First_care]
