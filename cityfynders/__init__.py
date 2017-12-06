3d976f1a8132c3d70cf716a585c560705ad0e54b
Zhuochen Han <zchan@uw.edu>
2017-12-05 22:48:35 -0800

    change __init__ and add setup.py

--- cityfynders/__init__.py
+++ cityfynders/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "1.0"
 
-from .data_processing import *
-from .plotly_usmap import *
-from .UI_setup import *
+from . import data_processing
+from . import plotly_usmap
+from . import UI_setup
