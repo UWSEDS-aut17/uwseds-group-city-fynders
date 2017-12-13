bf9a98104871406b687f1b2656016121023d3cc1
Zhuochen Han <zchan@uw.edu>
2017-12-12 18:27:35 -0800

    add tests and script

--- cityfynders/tests/test_usmap.py
+++ cityfynders/tests/test_usmap.py
@@ -1,2 +1,15 @@
-mport unittest
-import numpy as np
+import unittest
+import pandas as pd
+from cityfynders.plotly_usmap import usmap
+
+class usmapget(unittest.TestCase):
+
+    # One shot test
+    def test_usmap(self):
+        runwell = True
+        rank = pd.read_csv("../../data/rank_file.csv")
+        usmap(rank)
+        self.assertTrue(runwell)
+
+if __name__ == '__main__':
+    unittest.main()
