20c8a5c818e2469fadc91ff3679f82fcbcbbb1f2
Emily Wendan Yan <emwyan@uw.edu>
2017-12-13 20:52:46 -0800

    revised test_usmap.py

--- cityfynders/tests/test_usmap.py
+++ cityfynders/tests/test_usmap.py
@@ -12,6 +12,16 @@ class usmapget(unittest.TestCase):
         self.assertTrue(runwell)
 
     # def test_usmap_single_point
+    def test_newdf(self):
+        rank = pd.read_csv("../../data/rank_file.csv")
+        df = newdf(rank,'Air','Water','Toxics',
+         'Hazardous','Green_score')
+
+        for index, row in df.iterrows():
+            if row['City']=="Portland":
+                target = row['Total']
+
+        self.assertTrue(target == 1.0)
 
 if __name__ == '__main__':
     unittest.main()
